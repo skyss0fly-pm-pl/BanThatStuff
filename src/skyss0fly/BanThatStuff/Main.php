c8223abc44d9cc8912982b9807ce6469bca2401d
skyss0fly-pm-pl <144737802+skyss0fly-pm-pl@users.noreply.github.com>
2024-01-20 11:34:54 +1000

    Update Main.php

--- src/skyss0fly/BanThatStuff/Main.php
+++ src/skyss0fly/BanThatStuff/Main.php
@@ -12,7 +12,7 @@ class Main extends PluginBase {
   const $items = $cfg("BannedItems");
 
   public function onLoad(): void {
-   $this->getLogger()->info("Welcome to BanThatStuff \nThis is A Quick Guide to Usage: \nCommands: \n/banitem : Allows you to Ban the Item Held in your hand or By inputting the Id as an Arg \n/banword : Allows you to ban A Word. e.g /banword noob \nPlugin Constructed by skyss0fly");
+   $this->getLogger()->info("Welcome to BanThatStuff \nThis is A Quick Guide to Usage: \nCommands: \n/banitem : Allows you to Ban the Item Held in your hand or By inputting the Id as an Arg \n/banword : Allows you to ban A Word. e.g /banword noob \nCommands Coming Soon, Please Manually input the Ids and Words for now! \nPlugin Constructed by skyss0fly");
   }
 
 public function OnChat(PlayerChatEvent $event) {
@@ -42,8 +42,36 @@ $plr = $event->getPlayer();
       }
   }
 
-  public function onCommand(CommandSender $sender, Array $args, string $str):bool {
+  
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+        if (!$sender instanceof Player) {
+            $this->getLogger()->warning("Please use this command in-game");
+            return false;
+        }
 
-  }
+        switch ($command->getName()) {
+          case "banitem" :
+          if ($args[1] === 0) {
+            
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+        if (!$sender instanceof Player) {
+            $this->getLogger()->warning("Please use this command in-game");
+            return false;
+        }
+
+        
+
+        switch ($command->getName()) {
+            case "banitem":
+          $sender->sendMessage("Coming Soon. Please Manually Input the Ids in config");
+            #   if ($args[1] = 0) {
+                 
+       #    $sender->sendMessage("Error: Please input an Id!");
+      #    }
+     #     else {
+      #      $itemid = $args[1];
+        #    $items()->write($args[1]);
+       #     $sender->sendMessage("Successfully Banned The Item: " . VanillaItems()->GetItem($args[1])->name());
     
-}
+# }
+          
